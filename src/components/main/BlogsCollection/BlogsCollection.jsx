922930d2ef260a268dd678d6d987ceaa0cdb402c
markovo4 <obf2007@gmail.com>
2024-10-09 19:00:20 +0300

    partial completion of the slider section

--- src/components/main/BlogsCollection/BlogsCollection.jsx
+++ src/components/main/BlogsCollection/BlogsCollection.jsx
@@ -1,38 +1,68 @@
 import React from "react";
-import SliderCard from "../../UI/crads/SliderCard/index.js";
 import {blogs} from "./blogs.js";
 import styles from './styles.module.scss';
-import ArrowLeft from "../../../assets/svg/ArrowLeft.jsx";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import SliderCard from "../../UI/crads/SliderCard";
 import ArrowRight from "../../../assets/svg/ArrowRight.jsx";
+import ArrowLeft from "../../../assets/svg/ArrowLeft.jsx"; // Fix the typo in the path
 
 const BlogsCollection = () => {
+    function SampleNextArrow(props) {
+        const {className, style, onClick} = props;
+        return (
+            <button
+                className={`${styles.button} ${className}`}
+                style={{...style, display: "flex", justifyContent: "center", alignItems: "center"}}
+                onClick={onClick}
+            >
+                <ArrowRight/> {/* Replace with your right arrow icon or component */}
+            </button>
+        );
+    }
+
+    function SamplePrevArrow(props) {
+        const {className, style, onClick} = props;
+        return (
+            <button
+                className={`${styles.button} ${className}`}
+                style={{...style, display: "flex", justifyContent: "center", alignItems: "center"}}
+                onClick={onClick}
+            >
+                <ArrowLeft/> {/* Replace with your left arrow icon or component */}
+            </button>
+        );
+    }
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplaySpeed: 5000,
+        autoplay: true,
+        nextArrow: <SampleNextArrow/>, // Use the styled next arrow
+        prevArrow: <SamplePrevArrow/>, // Use the styled previous arrow
+    };
+
     return (
         <section className={styles.section}>
             <div className={styles.wrapper}>
-                <div className={styles.container}>
-                    {blogs.map((blog, index) => {
-                        return (
+                <Slider {...settings}>
+                    {blogs.map((blog, index) => (
+                        <div key={index} className={styles.sliderItem}>
                             <SliderCard
                                 title={blog.title}
                                 image={blog.image}
-                                key={index}
                             />
-                        )
-                    })}
-                </div>
-
-                <div className={styles.buttonGroup}>
-                    <button className={styles.button}>
-                        <ArrowLeft/>
-                    </button>
-                    <button className={styles.button}>
-                        <ArrowRight/>
-                    </button>
-                </div>
+                        </div>
+                    ))}
+                </Slider>
             </div>
-
         </section>
-    )
-}
+    );
+};
 
 export default BlogsCollection;
--- src/components/main/BlogsCollection/blogs.js
+++ src/components/main/BlogsCollection/blogs.js
@@ -7,5 +7,20 @@ export const blogs = [
         title: 'Printify Blog',
         image: {path: 'p1', alt: 'Printify Blog'},
     },
-
+    {
+        title: 'Printify Blog',
+        image: {path: 'p1', alt: 'Printify Blog'},
+    },
+    {
+        title: 'Printify Blog',
+        image: {path: 'p1', alt: 'Printify Blog'},
+    },
+    {
+        title: 'Printify Blog',
+        image: {path: 'p1', alt: 'Printify Blog'},
+    },
+    {
+        title: 'Printify Blog',
+        image: {path: 'p1', alt: 'Printify Blog'},
+    },
 ]
