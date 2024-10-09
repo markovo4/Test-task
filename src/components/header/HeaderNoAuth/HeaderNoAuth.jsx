9602df993d219f4f47ecfb7f22825580997db283
markovo4 <obf2007@gmail.com>
2024-10-09 12:22:23 +0300

    HeaderNoAuth addition

--- src/components/header/HeaderNoAuth/HeaderNoAuth.jsx
+++ src/components/header/HeaderNoAuth/HeaderNoAuth.jsx
@@ -6,26 +6,28 @@ import styles from './styles.module.scss'
 
 const HeaderNoAuth = () => {
     return (
-        <div className={styles.container}>
-            <Logo width={'129'} height={'89'}/>
+        <section className={styles.section}>
+            <div className={styles.container}>
+                <Logo width={'129'} height={'89'}/>
 
-            <nav className={styles.navContainer}>
-                <ul className={styles.linksContainer}>
-                    <li className={styles.navLink}><Link to={routerNames.pageHome}>Home</Link></li>
-                    <li className={styles.navLink}><Link to={routerNames.pageHome}>Success Stories</Link></li>
-                    <li className={styles.navLink}><Link to={routerNames.pageHome}>Services</Link></li>
-                    <li className={styles.navLink}><Link to={routerNames.pageHome}>Blog</Link></li>
-                    <li className={styles.navLink}><Link to={routerNames.pageHome}>About us</Link></li>
-                    <li className={styles.navLink}><Link to={routerNames.pageHome}>Career</Link></li>
-                </ul>
+                <nav className={styles.navContainer}>
+                    <ul className={styles.linksContainer}>
+                        <li className={styles.navLink}><Link to={routerNames.pageHome}>Home</Link></li>
+                        <li className={styles.navLink}><Link to={routerNames.pageHome}>Success Stories</Link></li>
+                        <li className={styles.navLink}><Link to={routerNames.pageHome}>Services</Link></li>
+                        <li className={styles.navLink}><Link to={routerNames.pageHome}>Blog</Link></li>
+                        <li className={styles.navLink}><Link to={routerNames.pageHome}>About us</Link></li>
+                        <li className={styles.navLink}><Link to={routerNames.pageHome}>Career</Link></li>
+                    </ul>
 
-                <div className={styles.buttonGroup}>
-                    <button className={styles.button}>Sign Up</button>
-                    <button className={styles.button}>Sign In</button>
-                </div>
-            </nav>
+                    <div className={styles.buttonGroup}>
+                        <button className={styles.button}>Sign Up</button>
+                        <button className={styles.button}>Sign In</button>
+                    </div>
+                </nav>
+            </div>
+        </section>
 
-        </div>
     )
 }
 
